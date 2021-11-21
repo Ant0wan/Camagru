af230e488195f8fc8b2b236746dd3c49497a2b55
Antoine Barthelemy <antoine@linux.com>
2021-11-21 16:08:00 +0100

    feat(server): try index.html serving

--- server/main.rs
+++ server/main.rs
@@ -14,10 +14,15 @@ fn main() {
 
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
-
     stream.read(&mut buffer).unwrap();
 
-    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    let contents = fs::read_to_string("../app/public/hello.html").unwrap();
+
+    let response = format!(
+        "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
+        contents.len(),
+        contents
+    );
 
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
