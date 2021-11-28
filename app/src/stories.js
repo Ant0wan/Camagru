329883be5f7f3c25d9c8cf3d8842f5e1119a7ba1
Antoine Barthelemy <antoine@linux.com>
2021-11-28 21:56:39 +0100

    feat(app): try include dynamic areas with javascript

---
+++ app/src/stories.js
@@ -0,0 +1,3 @@
+var stories = document.getElementById("stories");
+
+stories.innerHTML = '<div class="status-wrapper"><div class="status-card"><div class="profile-pic"><img src="258769440_1060473394771516_6457847328367936362_n.jpg"></div><p class="username"><div id="card-usrname">user_name_1</div></p><script src="../src/status.js"></script></div></div>'
