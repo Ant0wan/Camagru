c0aa731c25eea6337e626f225b19f81ae9b5c07f
Antoine Barthelemy <antoine@linux.com>
2021-11-28 21:50:23 +0100

    feat(app): try implement JS

---
+++ app/src/status.js
@@ -0,0 +1,4 @@
+var card_usrname = document.getElementById("card-usrname");
+card_usrname.innerHTML = "Ant0wan";
+
+//stories.innerHTML = '<div class="status-card"><div class="profile-pic"><img src="258769440_1060473394771516_6457847328367936362_n.jpg"></div><p class="username">user_name_1</p></div>';
